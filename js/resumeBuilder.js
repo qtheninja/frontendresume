8d96cb03129230f5893734cb6c00e41df4656cdc
Cody <meanderingmonarch@gmail.com>
2015-07-19 12:04:52 -0500

    Bug Fix: added new code to fix the problems with the js

---
+++ js/resumeBuilder.js
@@ -0,0 +1,11 @@
+$("#header").append();
+
+$("#main").append('Cody');
+
+var awesomeThoughts = "I am awesome";
+
+console.log(awesomeThoughts);
+
+var email = "myemail@email.com";
+var funThoughts = awesomeThoughts.replace("Awesome", "Fun");
+$("#main").append(funThoughts);
