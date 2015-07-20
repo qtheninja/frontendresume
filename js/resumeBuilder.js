6379eecf42c28a0dc0f82ffa8763374e3c0a6a9b
Cody <meanderingmonarch@gmail.com>
2015-07-19 21:22:38 -0500

    Changing computers: made some changes to json and added a display function outside of json for now

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,10 +1,11 @@
-//SKILL ARRAY FOR BIO
-var skills = [
+//SKILLS ARRAY FOR BIO
+var skills = 
+[
   "awesomeness",
   "programming",
   "teaching",
   "JS"
-  ];
+]
 //CONTACT OBJECT FOR BIO
 var contacts = {
   "mobile": "8675309",
@@ -24,7 +25,7 @@ var onlineCourses = [
 ];
 //BIO
 var bio = {
-    "name": "Cody",
+    "name": "Cody Higdem",
     "age": 30,
     "role": "Web Deveoper",
     "email": "codyhigdem@gmail.com",
@@ -34,24 +35,24 @@ var bio = {
     "skills": skills
   };
 
-  var education = {
-    "schools" : [
-      {
-        "name": "St Paul College",
-        "city": "St Paul Mn",
-        "degree": "AA",
-        "major": "N/A",
-        "year": 2009
-      },
-      {
-        "name": "University of Minnesota",
-        "city": "Minneapolis, Mn",
-        "degree": "BA",
-        "major": "Psychology",
-        "year": 2011
+var education = {
+  "schools" : [
+    {
+      "name": "St Paul College",
+      "city": "St Paul Mn",
+      "degree": "AA",
+      "major": "N/A",
+      "year": 2009
+    },
+    {
+      "name": "University of Minnesota",
+      "city": "Minneapolis, Mn",
+      "degree": "BA",
+      "major": "Psychology",
+      "year": 2011
       }
     ],
-    "onlineCourses": onlineCourses
+  "onlineCourses": onlineCourses
   };
   
 var work = {
@@ -63,14 +64,14 @@ var work = {
       "dates": "2009-2015",
       "description": "I was on a boat"
       },
-          {
+      {
       "employer": "myself",
       "title": "Bossman",
       "location": "Minneapolis, mn",
       "dates": "2023-2015",
       "description": "I was on a boat"
       },
-          {
+      {
       "employer": "myself",
       "title": "Bossman",
       "location": "Minneapolis, mn",
@@ -82,20 +83,95 @@ var work = {
 
 var projects = {
   "projects": [
- {
-   "title": "dances with wolves",
-   "dates": "1 - 100",
-   "description": "We danced inthe moon light",
-   "images": ["url here", "url here", "hurl"]
- } 
-]
+    {
+      "title": "dances with wolves",
+      "dates": "1 - 100",
+      "description": "We danced inthe moon light",
+      "images": ["url here", "url here", "hurl"]
+    } 
+  ]
 };
 
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+$("#header").append(formattedName);
+
+
 if (bio.skills.length > 0){
+
   $("#header").append(HTMLskillsStart);
-  var formattedSkill = HTMLskills.replace("%data%", bio.skills[0]);
-  $("#skills").append(formattedSkill);
-  console.log('you have skills sun');
-  }else{
-  console.log('you got no skilsl homes');
+
+  for (var i=0; i < bio.skills.length; i++)
+  {
+    var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+    $("#skills").append(formattedSkill);
+    console.log(formattedSkill);
+    console.log(bio.skills[i]);
+  };
+};
+
+
+
+
+
+for (skill in bio.skills){
+  console.log(bio.skills[skill])
 }
+
+var displayWork = function(){
+    //REMEMBER using for makes jobs the iterator
+for (job in work.jobs){
+  //HTMLworkStart is coming from helper.js
+  $("#workExperience").append(HTMLworkStart);
+  //MAKE THE FORMATTED SKILLS SETUP...
+  //We need to setup work.jobs[arrayNumber].employer look at the trail from var work/"jobs"/job-number/key_you_want in this case employer.
+  var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+  //then the title for the work object
+  var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+    var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+    var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+      var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+  //THEN concat for easier reading
+  var formattedEmployerTitle = formattedEmployer + formattedTitle + formattedLocation + formattedDates + formattedDescription;
+  //APPEND to work entry
+  $(".work-entry:last").append(formattedEmployerTitle);
+}
+};
+
+displayWork();
+
+
+
+$(document).click(function(loc){
+  var y = loc.pageY;
+  var x = loc.pageX;
+
+  logClicks(x,y);
+});
+
+var locationizer = function (work_obj){
+  var locations = [];
+
+  for (job in work_obj.jobs) {
+    var location = work_obj.jobs[job].location;
+    locations.push(location);
+  };
+  return locations;
+  /*
+  console.log('test');
+  for (var job in work.jobs){
+  locations.push(work.jobs[job].location);
+  }
+  return location;
+  */
+};
+
+var inName = function(name){
+  name = name.trim().split(" ");
+  name[1] = name[1].toUpperCase();
+  name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+   return name[0] + " " + name[1];
+
+}
+
+$('#main').append(internationalizeButton);
+console.log(locationizer(work));
